8b21401a3ee54dcf43842a829f6616068b44f5ab
mseshi <mseshidladla@gmail.com>
2021-07-26 17:34:25 +0200

    gege

--- afro/urls.py
+++ afro/urls.py
@@ -1,17 +1,26 @@
 from django.urls import path
 from django.conf.urls import url
 from . import views
+from .views import Register, Login
 from django.contrib.auth import views as auth_views
 
 
 app_name = 'afro'
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.home, name='home'),
+    url(r'^login/$', views.signin, name='login'),
+    url(r'^check-mail-ajax/$', views.check_mail_ajax, name='check_mail_ajax'),
     path('about-us/', views.about, name='aboutus'),
+    url(r'^register/$', Register.as_view(), name='register'),
+    url(r'login-req', Login.as_view(), name='login_ajax'),
     path('more-info/<str:pk>/', views.info, name='more-info'),
 
-    path('register/', views.register, name='register'),
-    path('login/', views.login1, name='login'),
-    path('logout/', views.logoutUser, name='logout'),
+    url(r'^subscription/', views.subscription, name='subscription'),
+    url(r'^subscribe/', views.subscribe, name='subscribe'),
+    url(r'^subscribed/', views.subscribed, name='subscribed'),
+    url(r'^sub/', views.end_sub, name='sub'),
+
+    url(r'^payment/$', views.call_back_url, name='payment'),
+
 
 ]    
